4f9f94d8c64fb790c42bbddf22b968b419e000bd
Jose A. Jimenez <jose.jimenez@liferay.com>
2017-03-07 12:12:56 +0100

    add custom methods to the impl

--- maven-6.2/portlets/WebServiceSample/WebServiceSample-portlet/src/main/java/com/jcampoy/liferay/samples/service/impl/LogServiceImpl.java
+++ maven-6.2/portlets/WebServiceSample/WebServiceSample-portlet/src/main/java/com/jcampoy/liferay/samples/service/impl/LogServiceImpl.java
@@ -1,6 +1,8 @@
 package com.jcampoy.liferay.samples.service.impl;
 
 import com.jcampoy.liferay.samples.service.base.LogServiceBaseImpl;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 
 /**
  * The implementation of the log remote service.
@@ -17,9 +19,32 @@ import com.jcampoy.liferay.samples.service.base.LogServiceBaseImpl;
  * @see com.jcampoy.liferay.samples.service.LogServiceUtil
  */
 public class LogServiceImpl extends LogServiceBaseImpl {
-    /*
-     * NOTE FOR DEVELOPERS:
-     *
-     * Never reference this interface directly. Always use {@link com.jcampoy.liferay.samples.service.LogServiceUtil} to access the log remote service.
-     */
+
+	public void debug(String msg) {
+		if (_log.isDebugEnabled()) {
+			_log.debug(msg);
+		}
+	}
+
+	public void fatal(String msg) {
+		_log.fatal(msg);
+	}
+
+	public void info(String msg) {
+		if (_log.isInfoEnabled()) {
+			_log.info(msg);
+		}
+	}
+
+	public void warn(String msg) {
+		_log.warn(msg);
+	}
+
+	public void trace(String msg) {
+		if (_log.isTraceEnabled()) {
+			_log.trace(msg);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog("WebServiceSample");
 }
