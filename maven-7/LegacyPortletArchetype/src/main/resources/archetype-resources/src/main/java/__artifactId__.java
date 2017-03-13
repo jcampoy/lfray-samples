5135a4d0a6011078bd13ecc5270e193fa8ea9ec5
Jose A. Jimenez <jose.jimenez@liferay.com>
2017-03-13 17:02:33 +0100

    legacy portlet archetype

---
+++ maven-7/LegacyPortletArchetype/src/main/resources/archetype-resources/src/main/java/__artifactId__.java
@@ -0,0 +1,10 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+public class ${artifactId} extends MVCPortlet {
+
+}
