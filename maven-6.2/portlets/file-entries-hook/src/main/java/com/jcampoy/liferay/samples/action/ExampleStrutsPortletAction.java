a1c4983c9293f6e4fa54283114416a776f976f98
Jose A. Jimenez <jose.jimenez@liferay.com>
2019-04-16 09:58:37 +0200

    add struct action sample in a hook

---
+++ maven-6.2/portlets/file-entries-hook/src/main/java/com/jcampoy/liferay/samples/action/ExampleStrutsPortletAction.java
@@ -0,0 +1,26 @@
+package com.jcampoy.liferay.samples.action;
+
+import com.liferay.portal.kernel.struts.BaseStrutsAction;
+import com.liferay.portal.kernel.struts.StrutsPortletAction;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+public class ExampleStrutsPortletAction extends BaseStrutsAction {
+
+	public String execute(
+            HttpServletRequest request, HttpServletResponse response)
+                    throws Exception {
+
+
+		System.out.println("execute /sample/sampletest");
+
+		return return "/portal/sample.jsp";
+	}
+
+}
